a035c83bbfe6603d3f0bfbd4d93834d23e1b6f61
BrandonLCC <br.casas@duocuc.cl>
2025-05-06 04:11:50 -0400

    Conexion BD, Mostrar productos de la base de datos - 06-05

--- ferremaxProject/store/models.py
+++ ferremaxProject/store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Categorias(models.Model):
+    id_categoria = models.IntegerField(primary_key=True) 
+    nombre_categoria = models.CharField(max_length = 50)
+    imagen_categoria = models.ImageField(upload_to='categoria/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre_categoria    
+
+class Producto(models.Model):
+    nombre_producto = models.CharField(max_length = 50)
+    descripcion_producto = models.TextField()
+    precio_producto = models.IntegerField()
+    cantidad_producto = models.IntegerField(default=0)  
+    creacion_producto = models.DateTimeField(auto_now_add=True)
+    imagen_producto = models.ImageField(upload_to='producto/', null=True, blank=True)
+    id_categoria = models.ForeignKey(Categorias,on_delete=models.CASCADE, default=0)#Sin categoria
+    #id_almacen = models.ForeignKey(Almacenes, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.nombre_producto    
--- ferremaxProject/store/views.py
+++ ferremaxProject/store/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render, redirect
+from .models import Producto, Categorias
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    productos = Producto.objects.select_related().all()[:5]
+    return render(request, 'index.html', {'productos':productos} )
+
+def listaProductos(request):
+    return 0
+
+'''
+
+
+def listaProductos(request):
+      categoria = request.GET.get('id_categoria', '')  
+
+    if categoria:
+        productos = Producto.objects.filter(id_categoria=categoria).select_related('id_categoria') #filtramos por id de la cate
+    else:
+        categoria = None
+        productos = Producto.objects.all()  
+
+    return render(request, 'listaProductos.html', {'categoria':categoria,'productos':productos} )
+    '''
